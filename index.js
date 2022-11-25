28e16475b368ea1f0fbe6f17b928a7d03032e718
KarimCarl <cndungu109@gmail.com>
2022-11-25 23:15:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-
 // Write your code here
+
+let num1 = 15.5;
+let num2 = 4;
+
+let multiply = num1*num2;
+
+console.log(multiply);
+//returns 62
+
+//returns a random number (integer*not float)) greater than 0
+let random;
+random = Math.round(Math.random()*10);
+
+
+//mod
+let num3 = 68;
+let num4 = 8;
+let mod = num3 % num4;
+
+console.log(mod);
+
+
+//number in a set
+let max = Math.max(5, 10, 15, 20);
+console.log(max);
+//Math.max picks out the highest number in the list, which is 20.
+
